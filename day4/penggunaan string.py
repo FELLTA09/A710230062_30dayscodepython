8991bc53756641d31f15fb2cfdcc376894a90740
FELLTA09 <panjifelta@gmail.com>
2024-06-09 18:43:15 +0700

    Create penggunaan string.py

---
+++ day4/penggunaan string.py
@@ -0,0 +1,9 @@
+#satu contoh pwnggunaan string 
+pesan = "p info mabar?"
+print(pesan)
+
+#contoh penggabungan string
+nama_depan = "Pri"
+nama_belakang ="Supri"
+nama_lengkap = nama_depan + " " + nama_belakang
+print(nama_lengkap)

e74eb513fad4d65ec2a2804965881440c72a3118
FELLTA09 <panjifelta@gmail.com>
2024-06-16 08:35:33 +0700

    Create mencetak output.py

---
+++ Day02/mencetak output.py
@@ -0,0 +1,38 @@
+
+# Menampilkan pesan 
+print("Selamat datang ciihuuyyy!")
+
+# Pengenalan variabel
+nama_pengguna = "agus kopling"
+umur_pengguna = 19
+
+# cara menampilkan hasil variabel dengan fungsi print
+print("Nama:", nama_pengguna)
+print("Umur:", umur_pengguna)
+
+# Pengenalan tipe data dasar
+tipe_data_str = "Ini adalah string"
+tipe_data_int = 123 #integer untuk bilangan bulat
+tipe_data_float = 3.14 #untuk bilangan pecahan
+tipe_data_bool = True # untuk menampilkan true/false
+
+# Menampilkan tipe data
+print("Contoh tipe data string:", tipe_data_str)
+print("Contoh tipe data integer:", tipe_data_int)
+print("Contoh tipe data float:", tipe_data_float)
+print("Contoh tipe data boolean:", tipe_data_bool)
+
+# Melakukan operasi sederhana
+angka1 = 10
+angka2 = 20
+hasil_penjumlahan = angka1 + angka2
+
+# Menampilkan hasil operasi
+print("Hasil penjumlahan", angka1, "dan", angka2, "adalah", hasil_penjumlahan)
+
+# Memperkenalkan fungsi
+def sapa_pengguna(nama):
+    print("Halo,", nama, "! mabar kuy , cihuuy!")
+
+# Memanggil fungsi
+sapa_pengguna(nama_pengguna)

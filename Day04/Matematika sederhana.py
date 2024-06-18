2e9e138bf0ac174ce745745839c6e49a45f5773e
FELLTA09 <panjifelta@gmail.com>
2024-06-18 19:01:15 +0700

    Create Matematika sederhana.py

---
+++ Day04/Matematika sederhana.py
@@ -0,0 +1,19 @@
+# Angka yg di gunakan
+angka1 = 10
+angka2 = 5
+
+# Penjumlahan
+hasil_penjumlahan = angka1 + angka2
+print(f"Hasil penjumlahan {angka1} + {angka2} = {hasil_penjumlahan}")
+
+# Pengurangan
+hasil_pengurangan = angka1 - angka2
+print(f"Hasil pengurangan {angka1} - {angka2} = {hasil_pengurangan}")
+
+# Perkalian
+hasil_perkalian = angka1 * angka2
+print(f"Hasil perkalian {angka1} * {angka2} = {hasil_perkalian}")
+
+# Pembagian
+hasil_pembagian = angka1 / angka2
+print(f"Hasil pembagian {angka1} / {angka2} = {hasil_pembagian}")
